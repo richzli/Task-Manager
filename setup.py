227d3fa1f3b1b91b024d8a3c54f25790b91f92ce
Richard Li <richzli@gmail.com>
2019-08-16 17:20:33 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+def setup():
+    conn_task = sqlite3.connect("task.db")
+    conn_done = sqlite3.connect("done.db")
+
+    curs_task = conn_task.cursor()
+    curs_done = conn_done.cursor()
+
+    command = """
+    CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    priority INTEGER DEFAULT 0,
+    due_date INTEGER
+    );
+    """
+
+    curs_task.execute(command)
+    curs_done.execute(command)
+
+    conn_task.commit()
+    conn_done.commit()
+
+    conn_task.close()
+    conn_done.close()
+
+if __name__ == "__main__":
+    setup()
