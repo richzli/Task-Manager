f22435105ae63cdf554477a42fef1f2ef257c309
Richard Li <richzli@gmail.com>
2019-09-09 11:03:25 -0400

    no reason to have two different files

--- task-manager/setup.py
+++ task-manager/setup.py
@@ -1,38 +1,43 @@
 import sqlite3
 from os.path import exists
+from contextlib import closing
 
 def setup():
     if not exists("../data"):
         from os import makedirs
         makedirs("../data")
     
-    conn_task = sqlite3.connect("../data/task.db")
-    conn_done = sqlite3.connect("../data/done.db")
-
-    curs_task = conn_task.cursor()
-    curs_done = conn_done.cursor()
-    
-    command = """
-    CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    type TEXT DEFAULT "task",
-    description TEXT,
-    priority INTEGER DEFAULT 0,
-    sticky INTEGER DEFAULT 0,
-    due_date INTEGER
-    );
-    """
-    #persistent tasks: due date = NULL
-    
-    curs_task.execute(command)
-    curs_done.execute(command)
-    
-    conn_task.commit()
-    conn_done.commit()
-
-    conn_task.close()
-    conn_done.close()
+    with closing(sqlite3.connect("../data/tasks.db")) as conn:
+        curs = conn.cursor()
+        
+        command_task = """
+        CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        type TEXT DEFAULT "task",
+        description TEXT,
+        priority INTEGER DEFAULT 0,
+        sticky INTEGER DEFAULT 0,
+        due_date INTEGER
+        );
+        """
+        command_done = """
+        CREATE TABLE IF NOT EXISTS done (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        type TEXT DEFAULT "task",
+        description TEXT,
+        priority INTEGER DEFAULT 0,
+        sticky INTEGER DEFAULT 0,
+        due_date INTEGER
+        );
+        """
+        #persistent tasks: due date = NULL
+        
+        curs.execute(command_task)
+        curs.execute(command_done)
+        
+        conn.commit()
 
 if __name__ == "__main__":
     setup()
