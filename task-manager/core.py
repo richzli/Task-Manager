af9986290cafd8e72c619bb6d66649393eb21d92
Richard Li <richzli@gmail.com>
2019-09-12 15:52:45 -0400

    Added some functionality, but haven't tested yet.

--- task-manager/core.py
+++ task-manager/core.py
@@ -1,4 +1,73 @@
 import sqlite3
+from contextlib import closing
 
-def add_task_cmd(data):
+def add_task(data: dict) -> None:
+    """
+    Adds a task to the tasks table.
+
+    @param data A dictionary holding pairs of column names and values.
+    """
     
+    with closing(sqlite3.connect("../data/tasks.db")) as conn:
+        with closing(conn.cursor()) as curs:
+            fields = str(tuple(data.keys())).replace("'", "")
+            values = tuple(data.values())
+            insert = "(" + ("?,"*len(values))[:-2] + ")"
+
+            sql = "INSERT INTO tasks " + fields + " VALUES " + insert
+
+            curs.execute(sql, values)
+        conn.commit()
+
+def update_task(taskid: int, data: dict) -> None:
+    """
+    Updates a task in the tasks table.
+
+    @param taskid The ID of the task to be updated.
+    @param data A dictionary holding pairs of column names and values.
+    """
+    
+    with closing(sqlite3.connect("../data/tasks.db")) as conn:
+        with closing(conn.cursor()) as curs:
+            fields = tuple(data.keys())
+            values = tuple(data.values())
+            update = " = ?, ".join(fields) + " = ?"
+            
+            sql = "UPDATE tasks SET " + update + " WHERE id = ?"
+
+            curs.execute(sql, values + (taskid,))
+        conn.commit()
+
+def finish_task(taskid: int) -> None:
+    """
+    Moves a task from the tasks table to the done table.
+
+    @param taskid The ID of the task to be moved.
+
+    """
+
+    with closing(sqlite3.connect("../data/tasks.db")) as conn:
+        with closing(conn.cursor()) as curs:
+            sql1 = "INSERT INTO done SELECT * FROM tasks WHERE id=?"
+            sql2 = "DELETE FROM tasks WHERE id=?"
+
+            curs.execute(sql1, (taskid,))
+            curs.execute(sql2, (taskid,))
+        conn.commit()
+            
+def unfinish_task(taskid: int) -> None:
+    """
+    Moves a task from the done table to the tasks table.
+
+    @param taskid The ID of the task to be moved.
+
+    """
+
+    with closing(sqlite3.connect("../data/tasks.db")) as conn:
+        with closing(conn.cursor()) as curs:
+            sql1 = "INSERT INTO tasks SELECT * FROM done WHERE id=?"
+            sql2 = "DELETE FROM done WHERE id=?"
+
+            curs.execute(sql1, (taskid,))
+            curs.execute(sql2, (taskid,))
+        conn.commit()
--- task-manager/setup.py
+++ task-manager/setup.py
@@ -8,35 +8,33 @@ def setup():
         makedirs("../data")
     
     with closing(sqlite3.connect("../data/tasks.db")) as conn:
-        curs = conn.cursor()
-        
-        command_task = """
-        CREATE TABLE IF NOT EXISTS tasks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        type TEXT DEFAULT "task",
-        description TEXT,
-        priority INTEGER DEFAULT 0,
-        sticky INTEGER DEFAULT 0,
-        due_date INTEGER
-        );
-        """
-        command_done = """
-        CREATE TABLE IF NOT EXISTS done (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        type TEXT DEFAULT "task",
-        description TEXT,
-        priority INTEGER DEFAULT 0,
-        sticky INTEGER DEFAULT 0,
-        due_date INTEGER
-        );
-        """
-        #persistent tasks: due date = NULL
-        
-        curs.execute(command_task)
-        curs.execute(command_done)
-        
+        with closing(conn.cursor()) as curs:
+            command_task = """
+            CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            type TEXT DEFAULT "task",
+            description TEXT,
+            priority INTEGER DEFAULT 0,
+            sticky INTEGER DEFAULT 0,
+            due_date INTEGER
+            );
+            """
+            command_done = """
+            CREATE TABLE IF NOT EXISTS done (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            type TEXT DEFAULT "task",
+            description TEXT,
+            priority INTEGER DEFAULT 0,
+            sticky INTEGER DEFAULT 0,
+            due_date INTEGER
+            );
+            """
+            #persistent tasks: due date = NULL
+            
+            curs.execute(command_task)
+            curs.execute(command_done)
         conn.commit()
 
 if __name__ == "__main__":
