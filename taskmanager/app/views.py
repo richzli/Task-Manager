9e6b90c965a7b038710d0d2c17831ce999e9cbda
Richard Li <richzli@gmail.com>
2019-11-08 17:22:15 -0500

    messing with more flask/html/bootstrap/whatever

--- taskmanager/app/views.py
+++ taskmanager/app/views.py
@@ -2,14 +2,17 @@ from flask import render_template
 
 from taskmanager.app import app
 
-@app.route('/')
-def home():
-    return "Hello, World!" 
 
-@app.route("/index")
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/index/")
 def index():
     return render_template("index.html")
 
-@app.route("/about")
+
+@app.route("/about/")
 def about():
     return render_template("about.html")
