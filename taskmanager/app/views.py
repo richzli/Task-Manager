5dc3cb86b423f68b755a51c8d9d59a7477019791
Richard Li <richzli@gmail.com>
2019-11-07 22:27:07 -0500

    started flask testing. very cool!

--- taskmanager/app/views.py
+++ taskmanager/app/views.py
@@ -2,11 +2,13 @@ from flask import render_template
 
 from taskmanager.app import app
 
+@app.route('/')
+def home():
+    return "Hello, World!" 
 
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
-
 
 @app.route("/about")
 def about():
